b8595a4c7d0cc5b1a890710722af67120c02afcc
NoahZ34 <124742989+NoahZ34@users.noreply.github.com>
2026-07-31 10:52:27 -0400

    Update t03_mixedsign4bitmult.sv

--- verilog/rtl/team_projects/team_03/t03_mixedsign4bitmult.sv
+++ verilog/rtl/team_projects/team_03/t03_mixedsign4bitmult.sv
@@ -6,10 +6,3 @@ module mixedsign4bitmult(
     assign signextendedout = $signed(signedin1) * $signed({1'b0,unsignedin2});
 endmodule
 
-module addersigned16bit(
-    input logic signed [15:0] in1,
-    input logic signed [15:0] in2,
-    output logic signed [15:0] out
-); 
-    assign out = in1 + in2;
-endmodule
